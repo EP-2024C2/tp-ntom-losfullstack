27aab5fb50021f1000fdbdb97c81dfa82c1318a7
Sebastian-Paulluk <129323586+Sebastian-Paulluk@users.noreply.github.com>
2024-10-09 00:33:19 -0300

    Crear fabricante.controller.js
    Se creó el controller con algunas funciones

---
+++ src/controllers/fabricante.controller.js
@@ -0,0 +1,53 @@
+const { Fabricante } = require('../models/Fabricante')
+const fabricanteController = {}
+
+
+
+const getFabricantes = async (req, res) => {
+    try {
+        const fabricantes = await Fabricante.findAll()
+        res.status(200).json(fabricantes)
+    } catch (error) {
+        res.status(500).json({ error: "Error al obtener los fabricantes" });
+    }
+}
+fabricanteController.getFabricantes = getFabricantes
+
+
+
+const getFabricante = async (req, res) => {
+    const id = req.params.id
+
+    try {
+        const fabricante = Fabricante.findByPK(id)
+        
+        if (!fabricante) {
+            res.status(404).json({ error: 'Fabricante no encontrado'})
+        }
+        res.status(200).json(fabricante)
+
+    } catch (error) {
+        res.status(505).json({ error: 'Error al obtener el fabricante'})
+    }
+}
+fabricanteController.getFabricante = getFabricante
+
+
+
+const addFabricante = async (req, res) => {
+    const { nombre, direccion, numeroContacto, pathImgPerfil } = req.body
+    
+    try {
+        const fabricante = await Fabricante.create({
+            nombre,
+            direccion,
+            numeroContacto,
+            pathImgPerfil
+        })
+        
+        res.status(202).json(fabricante)
+    }
+
+}
+
+module.exports = fabricanteController
